a45cc86ffa3c2b16b77a545ec15e679982b77a63
lokesh-gundluru <lokeshgundluru28@gmail.com>
2025-10-20 21:33:59 +0530

    Commit

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { io } from "socket.io-client";
 import { jwtDecode } from "jwt-decode";
 
-const socket = io(import.meta.env.VITE_BACKEND_URL || "http://localhost:5000");
+const socket = io(import.meta.env.VITE_API_URL || "http://localhost:5000");
 
 export default function Chat() {
   const [message, setMessage] = useState("");
